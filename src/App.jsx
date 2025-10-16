5e6e8f2bf41343befa54314ab9e148d57ed0b11e
ItsTanseer <tanseer.ahmad.2007@gmail.com>
2025-10-16 22:38:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MISTForm from './MISTFORM.jsx' 
+import MISTForm from './MISTForm.jsx' 
 function App() {
   
 
